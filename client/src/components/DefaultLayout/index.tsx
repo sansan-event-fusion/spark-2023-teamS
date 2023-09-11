cf1d801d765e8b79fdc39e65dd3b50c2c7cf8022
Yuta-NoobCoder <ukiyutam4a1@gmail.com>
2023-09-11 20:34:25 +0900

    リダイレクトまわり実装

--- client/src/components/DefaultLayout/index.tsx
+++ client/src/components/DefaultLayout/index.tsx
@@ -1,11 +1,26 @@
 import { Box, CssBaseline } from "@mui/material";
 import { Header } from "../Header";
 import { DrawerWrapper } from "@/components/DrawerWrapper";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DefaultLayoutProps } from "./typs";
-
+import { User, getAuth, getIdToken, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/router";
 export const DefaultLayout = ({ children }: DefaultLayoutProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (!user) {
+        router.replace("/signin");
+        // ユーザ情報が取得できなければサインインページに遷移
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <Box height="100vh" display="flex" flexDirection="column">
       <Header onClickMenu={() => setIsOpen(true)} />
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AxiosProvider } from "@/components/AxiosProvider";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AxiosProvider>
+      <Component {...pageProps} />
+    </AxiosProvider>
+  );
 }
