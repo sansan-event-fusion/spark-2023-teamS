806f70adab42e2d527fbdfc6e51d8dfbd398b179
kohki miyama <kmx.111010@gmail.com>
2023-09-11 15:20:02 +0900

    [add] 購入者のスタートページを作成

---
+++ client/src/pages/buyer/Start/index.tsx
@@ -0,0 +1,76 @@
+import { Box, Button, Typography } from "@mui/material";
+import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { ja } from "date-fns/locale";
+
+import { DefaultLayout } from "@/components/DefaultLayout";
+
+export default function BuyerStart() {
+  return (
+    <main>
+      <DefaultLayout>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "90%",
+          }}
+        >
+          <Box>
+            <Typography
+              sx={{
+                marginLeft: "10px",
+                display: { xs: "flex" },
+                flexDirection: { xs: "column", sm: "" },
+                alignItems: { xs: "center" },
+              }}
+            >
+              一歩先を行く、新しい宅配体験
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              marginTop: "20px",
+              display: { xs: "flex", sm: "inline" },
+              flexDirection: { xs: "column", sm: "" },
+              alignItems: { xs: "center" },
+            }}
+          >
+            <LocalizationProvider
+              adapterLocale={ja}
+              dateAdapter={AdapterDateFns}
+            >
+              <DatePicker
+                slotProps={{ textField: { size: "small" } }}
+                sx={{
+                  background: "white",
+                  width: { xs: "200px", sm: "580px" },
+                  marginLeft: "30px",
+                }}
+              />
+            </LocalizationProvider>
+            <Button
+              size="small"
+              variant="outlined"
+              style={{
+                borderColor: "black",
+                background: "#014A8F",
+              }}
+              sx={{
+                color: "white",
+                borderRadius: "50px",
+                marginTop: { xs: "10px", sm: "0px" },
+                marginX: "10px",
+                width: "160px",
+                fontSize: "20px",
+              }}
+            >
+              検索
+            </Button>
+          </Box>
+        </Box>
+      </DefaultLayout>
+    </main>
+  );
+}
