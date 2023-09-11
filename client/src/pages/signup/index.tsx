d0700438261d36550e3f5d3b0e705b7e02370e94
kohki miyama <kmx.111010@gmail.com>
2023-09-11 22:20:12 +0900

    [add] ユーザー登録画面の作成

---
+++ client/src/pages/signup/index.tsx
@@ -0,0 +1,144 @@
+import Link from "next/link";
+
+import { Box, Button, TextField } from "@mui/material";
+
+export default function signup() {
+  return (
+    <main>
+      <Box
+        sx={{
+          width: "100%",
+        }}
+      >
+        <Box
+          sx={{
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <TextField
+            size="small"
+            defaultValue=""
+            id="name"
+            label="氏名"
+            variant="outlined"
+            sx={{
+              width: { xs: "240px", sm: "360px" },
+              marginBottom: "20px",
+            }}
+          />
+          <TextField
+            size="small"
+            defaultValue=""
+            id="email"
+            label="メールアドレス"
+            variant="outlined"
+            sx={{
+              width: { xs: "240px", sm: "360px" },
+              marginBottom: "20px",
+            }}
+          />
+          <TextField
+            size="small"
+            defaultValue=""
+            id="password"
+            label="パスワード"
+            variant="outlined"
+            type="password"
+            sx={{
+              width: { xs: "240px", sm: "360px" },
+
+              marginBottom: "20px",
+            }}
+          />
+          <TextField
+            size="small"
+            defaultValue=""
+            id="phone_number"
+            label="電話番号"
+            variant="outlined"
+            sx={{
+              width: { xs: "240px", sm: "360px" },
+              marginBottom: "20px",
+            }}
+          />
+          <TextField
+            size="small"
+            defaultValue=""
+            id="postal_code"
+            label="郵便番号"
+            variant="outlined"
+            sx={{
+              width: { xs: "240px", sm: "360px" },
+              marginBottom: "20px",
+            }}
+          />
+          <TextField
+            size="small"
+            defaultValue=""
+            id="prefecture"
+            label="都道府県"
+            variant="outlined"
+            sx={{
+              width: { xs: "240px", sm: "360px" },
+              marginBottom: "20px",
+            }}
+          />
+          <TextField
+            size="small"
+            defaultValue=""
+            id="city"
+            label="市町村"
+            variant="outlined"
+            sx={{
+              width: { xs: "240px", sm: "360px" },
+              marginBottom: "20px",
+            }}
+          />
+          <TextField
+            size="small"
+            defaultValue=""
+            id="address"
+            label="番地"
+            variant="outlined"
+            sx={{
+              width: { xs: "240px", sm: "360px" },
+              marginBottom: "20px",
+            }}
+          />
+          <TextField
+            size="small"
+            defaultValue=""
+            id="number"
+            label="部屋番号"
+            variant="outlined"
+            sx={{
+              width: { xs: "240px", sm: "360px" },
+              marginBottom: "20px",
+            }}
+          />
+          <Button
+            size="small"
+            variant="outlined"
+            style={{
+              borderColor: "black",
+              background: "#014A8F",
+            }}
+            sx={{
+              color: "white",
+              marginTop: { xs: "10px", sm: "0px" },
+              marginX: "10px",
+              width: "160px",
+              fontSize: "20px",
+            }}
+          >
+            <Link href={""}>登録</Link>
+          </Button>
+        </Box>
+      </Box>
+    </main>
+  );
+}
