caa409f1fbde59a07fe8de956879706dedd9a58a
kohki miyama <kmx.111010@gmail.com>
2023-09-12 12:30:46 +0900

    [update] シフト追加ボタンの作成

--- client/src/pages/agent/schedule/index.tsx
+++ client/src/pages/agent/schedule/index.tsx
@@ -2,13 +2,13 @@ import { DefaultLayout } from "@/components/DefaultLayout";
 import { DeliveryScheduleList } from "@/components/DeliveryScheduleList";
 import { DeliveryShiftList } from "@/components/DeliveryShiftList";
 import { UserCountList } from "@/components/UserCountList";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 
 export default function agentSchedulePage() {
   return (
     <DefaultLayout>
       <Box
-        height="100%"
+        height="80%"
         display="flex"
         flexDirection="column"
         alignItems="center"
@@ -32,6 +32,23 @@ export default function agentSchedulePage() {
           </Box>
         </Box>
       </Box>
+      <Button
+        size="small"
+        variant="outlined"
+        style={{
+          borderColor: "black",
+          background: "#014A8F",
+        }}
+        sx={{
+          color: "white",
+          marginTop: "10px",
+          marginX: "10px",
+          width: "160px",
+          fontSize: "20px",
+        }}
+      >
+        シフト追加
+      </Button>
     </DefaultLayout>
   );
 }
