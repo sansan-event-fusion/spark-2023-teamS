a5fd5801ef9ebdf10e1a2819abfefade0de967a7
Yuta-NoobCoder <ukiyutam4a1@gmail.com>
2023-09-13 11:19:44 +0900

    Providerに変わった部分を削除

--- client/src/components/DefaultLayout/index.tsx
+++ client/src/components/DefaultLayout/index.tsx
@@ -5,21 +5,9 @@ import { useEffect, useState } from "react";
 import { DefaultLayoutProps } from "./typs";
 import { User, getAuth, getIdToken, onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/router";
+import { app } from "../../lib/firebase";
 export const DefaultLayout = ({ children }: DefaultLayoutProps) => {
   const [isOpen, setIsOpen] = useState(false);
-  const auth = getAuth();
-  const user = auth.currentUser;
-  const router = useRouter();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (!user) {
-        router.replace("/signin");
-        // ユーザ情報が取得できなければサインインページに遷移
-      }
-    });
-    return () => unsubscribe();
-  }, []);
 
   return (
     <Box height="100vh" display="flex" flexDirection="column">
