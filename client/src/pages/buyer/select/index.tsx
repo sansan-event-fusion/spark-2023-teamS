0f1ea24d5ec5ed353eae13589aa3e1f17c09c793
kohki miyama <kmx.111010@gmail.com>
2023-09-13 19:42:15 +0900

    [update] 代理人のシフト情報取得処理を記述

--- client/src/pages/buyer/select/index.tsx
+++ client/src/pages/buyer/select/index.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
+
 import { AgentsList } from "@/components/AgentsList";
-import { DailyUserCountCard } from "@/components/DailyUserCountCard";
+
 import { DefaultLayout } from "@/components/DefaultLayout";
 import { UserCountList } from "@/components/UserCountList";
 import { Box } from "@mui/material";
+import { instance } from "@/components/AxiosProvider";
+
+import dayjs from "dayjs";
 
 export default function buyerSelectPage() {
+  const [shiftList, setShiftList] = useState();
+
+  const today = dayjs().format("YYYY-MM-DD");
+  const queryParams = {
+    date: today,
+  };
+
+  useEffect(() => {
+    const getShiftList = async () => {
+      await instance
+        .get("/shifts/agents", {
+          params: queryParams,
+        })
+        .then(({ data }) => setShiftList(data));
+    };
+    getShiftList();
+  }, []);
+
+  console.log(shiftList);
+
   return (
     <DefaultLayout>
       <Box
